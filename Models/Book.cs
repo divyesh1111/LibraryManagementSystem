71de5a92c302faeb2aba18991d516187c976acd8
divyesh1111 <divyeshhirpara000@gmail.com>
2026-01-31 18:44:51 -0800

    Add Book model with foreign keys to Author and LibraryBranch

---
+++ Models/Book.cs
@@ -0,0 +1,68 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(300)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "ISBN is required")]
+        [StringLength(20)]
+        [Display(Name = "ISBN")]
+        public string ISBN { get; set; } = string.Empty;
+
+        [StringLength(1000)]
+        public string? Description { get; set; }
+
+        [Display(Name = "Publication Date")]
+        [DataType(DataType.Date)]
+        public DateTime? PublicationDate { get; set; }
+
+        [StringLength(200)]
+        public string? Publisher { get; set; }
+
+        [Range(1, 10000)]
+        [Display(Name = "Page Count")]
+        public int? PageCount { get; set; }
+
+        [StringLength(50)]
+        public string? Genre { get; set; }
+
+        [Display(Name = "Cover Image URL")]
+        public string? CoverImageUrl { get; set; }
+
+        [Range(0, 1000)]
+        [Display(Name = "Available Copies")]
+        public int AvailableCopies { get; set; } = 1;
+
+        [Range(0, 1000)]
+        [Display(Name = "Total Copies")]
+        public int TotalCopies { get; set; } = 1;
+
+        [Display(Name = "Is Available")]
+        public bool IsAvailable => AvailableCopies > 0;
+
+        [Display(Name = "Created Date")]
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        // Foreign Keys
+        [Required]
+        [Display(Name = "Author")]
+        public int AuthorId { get; set; }
+
+        [Display(Name = "Library Branch")]
+        public int? LibraryBranchId { get; set; }
+
+        // Navigation Properties
+        [ForeignKey("AuthorId")]
+        public virtual Author? Author { get; set; }
+
+        [ForeignKey("LibraryBranchId")]
+        public virtual LibraryBranch? LibraryBranch { get; set; }
+    }
+}
