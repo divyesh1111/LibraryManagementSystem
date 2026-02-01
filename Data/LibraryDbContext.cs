503c8bdad11d013d7045a334e611df62a98dfc9a
divyesh1111 <divyeshhirpara000@gmail.com>
2026-01-31 18:48:08 -0800

    feat:Add LibraryDbContext with DbSet properties for all entities

---
+++ Data/LibraryDbContext.cs
@@ -0,0 +1,17 @@
+using LibraryManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<LibraryBranch> LibraryBranches { get; set; }
+    }
+}
