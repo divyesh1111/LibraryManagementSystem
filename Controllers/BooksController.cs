ba8af9b416162769914f54957d241571805c442c
divyesh1111 <divyeshhirpara000@gmail.com>
2026-02-03 09:54:57 -0800

    feat:Add Create and Edit actions with form views for Books

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -24,7 +24,6 @@ namespace LibraryManagementSystem.Controllers
                 .Include(b => b.LibraryBranch)
                 .AsQueryable();
 
-            // Search by title or ISBN
             if (!string.IsNullOrEmpty(searchString))
             {
                 booksQuery = booksQuery.Where(b => 
@@ -33,7 +32,6 @@ namespace LibraryManagementSystem.Controllers
                     (b.Author != null && (b.Author.FirstName.Contains(searchString) || b.Author.LastName.Contains(searchString))));
             }
 
-            // Filter by genre
             if (!string.IsNullOrEmpty(genre))
             {
                 booksQuery = booksQuery.Where(b => b.Genre == genre);
@@ -61,7 +59,6 @@ namespace LibraryManagementSystem.Controllers
                 })
                 .ToListAsync();
 
-            // Get list of genres for filter dropdown
             ViewBag.Genres = await _context.Books
                 .Where(b => b.Genre != null)
                 .Select(b => b.Genre)
@@ -114,6 +111,211 @@ namespace LibraryManagementSystem.Controllers
             };
 
             return View(viewModel);
+        }
+
+        // GET: Books/Create
+        public async Task<IActionResult> Create()
+        {
+            var viewModel = new BookCreateViewModel
+            {
+                Authors = new SelectList(
+                    await _context.Authors
+                        .OrderBy(a => a.LastName)
+                        .ThenBy(a => a.FirstName)
+                        .Select(a => new { a.Id, FullName = a.FirstName + " " + a.LastName })
+                        .ToListAsync(),
+                    "Id", "FullName"),
+                LibraryBranches = new SelectList(
+                    await _context.LibraryBranches
+                        .Where(b => b.IsActive)
+                        .OrderBy(b => b.BranchName)
+                        .ToListAsync(),
+                    "Id", "BranchName")
+            };
+
+            return View(viewModel);
+        }
+
+        // POST: Books/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(BookCreateViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Check if ISBN already exists
+                if (await _context.Books.AnyAsync(b => b.ISBN == viewModel.ISBN))
+                {
+                    ModelState.AddModelError("ISBN", "A book with this ISBN already exists.");
+                }
+                else
+                {
+                    var book = new Book
+                    {
+                        Title = viewModel.Title,
+                        ISBN = viewModel.ISBN,
+                        Description = viewModel.Description,
+                        PublicationDate = viewModel.PublicationDate,
+                        Publisher = viewModel.Publisher,
+                        PageCount = viewModel.PageCount,
+                        Genre = viewModel.Genre,
+                        CoverImageUrl = viewModel.CoverImageUrl,
+                        AvailableCopies = viewModel.AvailableCopies,
+                        TotalCopies = viewModel.TotalCopies,
+                        AuthorId = viewModel.AuthorId,
+                        LibraryBranchId = viewModel.LibraryBranchId,
+                        CreatedDate = DateTime.UtcNow
+                    };
+
+                    _context.Add(book);
+                    await _context.SaveChangesAsync();
+                    TempData["SuccessMessage"] = "Book created successfully!";
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            // Reload dropdown lists
+            viewModel.Authors = new SelectList(
+                await _context.Authors
+                    .OrderBy(a => a.LastName)
+                    .ThenBy(a => a.FirstName)
+                    .Select(a => new { a.Id, FullName = a.FirstName + " " + a.LastName })
+                    .ToListAsync(),
+                "Id", "FullName", viewModel.AuthorId);
+            viewModel.LibraryBranches = new SelectList(
+                await _context.LibraryBranches
+                    .Where(b => b.IsActive)
+                    .OrderBy(b => b.BranchName)
+                    .ToListAsync(),
+                "Id", "BranchName", viewModel.LibraryBranchId);
+
+            return View(viewModel);
+        }
+
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new BookEditViewModel
+            {
+                Id = book.Id,
+                Title = book.Title,
+                ISBN = book.ISBN,
+                Description = book.Description,
+                PublicationDate = book.PublicationDate,
+                Publisher = book.Publisher,
+                PageCount = book.PageCount,
+                Genre = book.Genre,
+                CoverImageUrl = book.CoverImageUrl,
+                AvailableCopies = book.AvailableCopies,
+                TotalCopies = book.TotalCopies,
+                AuthorId = book.AuthorId,
+                LibraryBranchId = book.LibraryBranchId,
+                Authors = new SelectList(
+                    await _context.Authors
+                        .OrderBy(a => a.LastName)
+                        .ThenBy(a => a.FirstName)
+                        .Select(a => new { a.Id, FullName = a.FirstName + " " + a.LastName })
+                        .ToListAsync(),
+                    "Id", "FullName", book.AuthorId),
+                LibraryBranches = new SelectList(
+                    await _context.LibraryBranches
+                        .Where(b => b.IsActive)
+                        .OrderBy(b => b.BranchName)
+                        .ToListAsync(),
+                    "Id", "BranchName", book.LibraryBranchId)
+            };
+
+            return View(viewModel);
+        }
+
+        // POST: Books/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, BookEditViewModel viewModel)
+        {
+            if (id != viewModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                // Check if ISBN already exists for another book
+                if (await _context.Books.AnyAsync(b => b.ISBN == viewModel.ISBN && b.Id != id))
+                {
+                    ModelState.AddModelError("ISBN", "A book with this ISBN already exists.");
+                }
+                else
+                {
+                    try
+                    {
+                        var book = await _context.Books.FindAsync(id);
+                        if (book == null)
+                        {
+                            return NotFound();
+                        }
+
+                        book.Title = viewModel.Title;
+                        book.ISBN = viewModel.ISBN;
+                        book.Description = viewModel.Description;
+                        book.PublicationDate = viewModel.PublicationDate;
+                        book.Publisher = viewModel.Publisher;
+                        book.PageCount = viewModel.PageCount;
+                        book.Genre = viewModel.Genre;
+                        book.CoverImageUrl = viewModel.CoverImageUrl;
+                        book.AvailableCopies = viewModel.AvailableCopies;
+                        book.TotalCopies = viewModel.TotalCopies;
+                        book.AuthorId = viewModel.AuthorId;
+                        book.LibraryBranchId = viewModel.LibraryBranchId;
+
+                        _context.Update(book);
+                        await _context.SaveChangesAsync();
+                        TempData["SuccessMessage"] = "Book updated successfully!";
+                        return RedirectToAction(nameof(Index));
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!await BookExists(viewModel.Id))
+                        {
+                            return NotFound();
+                        }
+                        throw;
+                    }
+                }
+            }
+
+            // Reload dropdown lists
+            viewModel.Authors = new SelectList(
+                await _context.Authors
+                    .OrderBy(a => a.LastName)
+                    .ThenBy(a => a.FirstName)
+                    .Select(a => new { a.Id, FullName = a.FirstName + " " + a.LastName })
+                    .ToListAsync(),
+                "Id", "FullName", viewModel.AuthorId);
+            viewModel.LibraryBranches = new SelectList(
+                await _context.LibraryBranches
+                    .Where(b => b.IsActive)
+                    .OrderBy(b => b.BranchName)
+                    .ToListAsync(),
+                "Id", "BranchName", viewModel.LibraryBranchId);
+
+            return View(viewModel);
+        }
+
+        private async Task<bool> BookExists(int id)
+        {
+            return await _context.Books.AnyAsync(e => e.Id == id);
         }
     }
 }
